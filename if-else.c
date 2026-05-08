8f066b8b923ff7d7a853697cb3de08695d52691b
Nimbu Paani <rajan085910@gmail.com>
2026-05-08 22:05:52 +0530

    if else

---
+++ if-else.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10, b = 30;
+
+    if (a < b)
+    {
+        printf("%d \n", a);
+    }
+    else
+    {
+        printf("%d \n", b);
+    }
+    return 0;
+}
